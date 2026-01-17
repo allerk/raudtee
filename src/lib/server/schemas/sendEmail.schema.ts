4b99b44a8bbb00778781e64d32af7a89bb4cb9f9
aleksandr.lerko <alexandr.lerko@gmail.com>
2026-01-17 23:30:44 +0200

    feat: update schema validator to include files

--- src/lib/server/schemas/sendEmail.schema.ts
+++ src/lib/server/schemas/sendEmail.schema.ts
@@ -1,4 +1,7 @@
 import { z } from 'zod';
+
+const MAX_FILE_SIZE = 4 * 1024 * 1024;
+
 export const sendEmailSchema = z.object({
 	subject: z.string().min(1, 'Subject must not be empty').max(200),
 	content: z.string().min(1, 'Content must not be empty'),
@@ -7,4 +10,9 @@ export const sendEmailSchema = z.object({
 	)
 		.min(1, 'At least one recipient must be provided')
 		.max(3, 'Maximum of 3 recipients allowed'),
+	attachments: z.array(
+		z.instanceof(File)
+			.refine((file) => file.size <= MAX_FILE_SIZE, 'Max file size is 4MB')
+		// might be useful to validate the file type later
+	).optional()
 });
